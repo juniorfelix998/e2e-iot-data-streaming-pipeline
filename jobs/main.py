d5eb05928e9efac481b86efd19cee4971fe6a107
felix <felixokoth5@gmail.com>
2024-04-02 00:01:58 +0300

    simulate vehicle information data

--- jobs/main.py
+++ jobs/main.py
@@ -1,12 +1,19 @@
 import os
+import logging
+import random
+import uuid
+
+import simplejson as json
 
 from confluent_kafka import SerializingProducer
-import simplejson as json
+from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 
 SAN_FRANCISCO_COORDINATES = {"latitude": 37.773972, "longitude": -122.431297}
@@ -28,3 +35,67 @@ GPS_TOPIC = os.getenv("GPS_TOPIC", "gps_data")
 TRAFFIC_TOPIC = os.getenv("TRAFFIC_TOPIC", "traffic_data")
 WEATHER_TOPIC = os.getenv("WEATHER_TOPIC", "weather_data")
 EMERGENCY_TOPIC = os.getenv("EMERGENCY_TOPIC", "emergency_data")
+
+start_time = datetime.now()
+start_location = SAN_FRANCISCO_COORDINATES.copy()
+
+
+def get_next_time():
+    global start_time
+
+    start_time += timedelta(seconds=random.randint(30, 60))
+    return start_time
+
+
+def simulate_vehicle_movement():
+    global start_location
+
+    # move to new york
+    start_location["latitude"] += LATITUDE_INCREMENT
+    start_location["longitude"] += LONGITUDE_INCREMENT
+
+    # adding randomness
+
+    start_location["latitude"] += random.uniform(-0.0005, 0.0005)
+    start_location["longitude"] += random.uniform(-0.0005, 0.0005)
+    return start_location
+
+
+def generate_vehicle_data(device_id):
+    location = simulate_vehicle_movement()
+
+    return {
+        "id": uuid.uuid4(),
+        "device_id": device_id,
+        "timestamp": get_next_time().isoformat(),
+        "location": (location["latitude"], location["longitude"]),
+        "speed": random.uniform(10, 80),
+        "direction": "North-East",
+        "make": "Mercedes",
+        "model": "C200",
+        "year": 2020,
+        "fuel_type": "hybrid",
+    }
+
+
+def simulate_journey(producer, device_id):
+    while True:
+        vehicle_data = generate_vehicle_data(device_id)
+        print(vehicle_data)
+        break
+
+
+if __name__ == "__main__":
+    producer_config = {
+        "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
+        "error_cb": lambda err: logger.error(f"Kafka error:{err}"),
+    }
+
+    producer = SerializingProducer(producer_config)
+
+    try:
+        simulate_journey(producer, "Vehicle-1")
+    except KeyboardInterrupt:
+        logger.error("Simulation stopped by user")
+    except Exception as e:
+        logger.exception(f"An Exception occurred; {e}")
