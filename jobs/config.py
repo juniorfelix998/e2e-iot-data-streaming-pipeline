e6d7140de4e943a642ad4001f5ee0cd0ac7b009a
felix <felixokoth5@gmail.com>
2024-04-02 13:36:15 +0300

    add config files , set up spark to connect to s3

---
+++ jobs/config.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+AWS_ACCESS_KEY = os.environ.get("AWS_SECRET_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_ACCESS_KEY")
+
+configuration = {
+    "AWS_SECRET_KEY": AWS_SECRET_KEY,
+    "AWS_ACCESS_KEY": AWS_ACCESS_KEY,
+}
---
+++ jobs/iot-stream.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+from config import configuration
+
+
+def main():
+    spark = (
+        SparkSession.builder.appName("IotStreaming")
+        .config(
+            "spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.1,"
+            "org.apache.hadoop:hadoop-aws:3.3.1,"
+            "com.amazonaws:aws-java-sdk:1.11.469",
+        )
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        .config("spark.hadoop.fs.s3a.access.key", configuration.get("AWS_ACCESS_KEY"))
+        .config("spark.hadoop.fs.s3a.secret.key", configuration.get("AWS_SECRET_KEY"))
+        .config(
+            "spark.hadoop.fs.s3a.aws.credentials.provider",
+            "org.apache.hadoop.fs.s3a.impl.SimpleAWSCredentialsProvider",
+        ).getOrCreate()
+    )
+
+    # log level to minimize console output
+    spark.sparkContext.setLogLevel("WARN")
+
+
+if __name__ == "__main__":
+    main()
